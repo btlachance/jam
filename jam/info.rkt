9c59923d38bdc66b59a7bfa47741049aed7b5341
Brian Lachance <blachance@gmail.com>
2019-04-30 11:24:42 -0400

    Omit more python noise

--- jam/info.rkt
+++ jam/info.rkt
@@ -1,6 +1,7 @@
 #lang info
 (define compile-omit-paths
   '("private/python/pycket"
+    "private/python/__pycache__"
     "private/python/core.py"
     "private/python/json_adapter.py"
     "private/python/target.py"))
